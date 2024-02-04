9cc7d3e01c7385cf659dbe91b7c7096dbdd86651
b0xtch <botchm@hotmail.com>
2024-02-03 19:23:23 -0800

    new images and contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://wrapitmoving.com",
   integrations: [tailwind(), sitemap(), icon()],
+  output: "server",
+  adapter: vercel()
 });
---
+++ src/pages/api/send.ts
@@ -0,0 +1,39 @@
+import type { APIRoute } from 'astro'
+import sendGrid from '@sendgrid/mail';
+
+const key = import.meta.env.SENDGRID_API_KEY;
+
+sendGrid.setApiKey(key);
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+    try{
+      // Getting posted data from our contact form
+      const data = await request.formData();
+      const name = data.get('name');
+      const email = data.get('email');
+      const phone = data.get('phone');
+      const move_date = data.get('move_date');
+      const additional_info = data.get('additional_info');
+    
+        // Sending email
+        const msg = {
+            // to: 'info.wrapitmoving@gmail.com', // Change to your recipient
+            to: 'botchxyz@gmail.com', // Change to your recipient
+            from: 'info.wrapitmoving@gmail.com', // Change to your verified sender
+            subject: 'Quote Submission from website',
+            text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMove Date: ${move_date}\n\n${additional_info}`,
+            html: `Name: ${name}<br/>Email: ${email}<br/>Phone: ${phone}<br/>Move Date: ${move_date}<br/><br/>${additional_info}`,
+        }
+        
+        await sendGrid.send(msg).then(() => {
+          console.log('Email sent')
+        }).catch((error) => {
+          console.error(error)
+        });
+    
+      } catch (error) {
+        console.error(error);
+      }
+
+    return redirect(`/`);
+}
